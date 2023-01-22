9d6db96a39a35811d27c59cbe6bed2dd3844d51d
Luis Angel <angelfgdeveloper@gmail.com>
2023-01-22 14:04:51 -0600

    Fetch con promesas

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,22 @@
+
+const apiKey = 'EXnq69FyQDXiconiDW07qvK2UjrY2nuI';
+
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+// promesas en cadena
+peticion
+  .then((resp) => resp.json())
+  .then(({ data }) => {
+    const { url } = data.images.original;
+
+    const img = document.createElement('img');
+    img.src = url;
+
+    document.body.append(img);
+
+  })
+  .catch(console.warn);
+
+
+
