f889443fd266c0247c7ba59164393f144041bc82
Luis Angel <angelfgdeveloper@gmail.com>
2023-01-22 14:23:08 -0600

    Ternario

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,21 @@
+
+const activo = true;
+
+// let mensaje = '';
+
+// if (activo) {
+//   mensaje = 'Activo';
+// } else {
+//   mensaje = 'Inactivo';
+// }
+
+// Ternario
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+// const mensaje = ( activo ) ? 'Activo' : null;
+
+// Operador and &&
+const mensaje = activo && 'Activo'; // Si es !activo => regresa false
+
+console.log(mensaje);
+
+
